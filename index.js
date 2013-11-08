b7f06dac1b7b401988fd21cc2d181bea89824a52
Remington A. Holt <holtremington@gmail.com>
2013-11-08 17:41:27 -0500

    Sprite Animation Outline
    Good Evening everybody!! LIVE FROM GOOGLE DC

---
+++ index.js
@@ -0,0 +1,43 @@
+var stage = new Kinetic.stage({
+	container: 'gamecontainer'
+});
+
+var layer = new Kinetic.layer();
+
+var animations = {
+	idle: [{}],
+	run: [{}],
+	jump: [{}]
+};
+
+var imageObj = new Image();
+
+imageObj.onload = function(){
+	var chero = new Kinetic.Sprite({
+		//x-position,
+		//y-position,
+		//image name
+		//default animation
+		//possible animations
+		//frame rate 
+		//index no of default animation
+	
+		//Add the shape to the layer
+		
+		
+		//Add the laer to the stage
+		
+		
+		//start sprite animation
+		
+		
+		//resume transition
+	
+	
+	});
+
+//imageObj.src = '/path/to/image'
+
+
+
+};
